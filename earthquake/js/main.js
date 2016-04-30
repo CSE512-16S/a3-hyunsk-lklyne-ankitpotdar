e32654e6d527ce9bb30294acc481b45540a005c0
Hyun S Kim <hyunkims@gmail.com>
2016-04-29 21:44:33 -0700

    normalize features array to dataMap format. draw bubbles

--- earthquake/js/main.js
+++ earthquake/js/main.js
@@ -1,20 +1,37 @@
 var quakeData = null;
+var features = [];
 
 var worldMap = {
   svg: null,
   init: function(){
-    worldMap.svg = new Datamap({
+    worldMap.dataMap = new Datamap({
       element: $(".map")[0]
     });
+    worldMap.drawBubbles();
   },
   drawBubbles: function(){
+    worldMap.dataMap.bubbles(features);
   }
+}
+
+function normalizeFeatures(){
+  _.forEach(quakeData.features, function(val, i){
+    var feature = val.properties;
+    feature.id = val.id;
+    feature.latitude = val.geometry.coordinates[1];
+    feature.longitude = val.geometry.coordinates[0];
+    feature.depth = val.geometry.coordinates[2];
+
+    feature.radius = val.properties.mag;
+
+    features.push(feature);
+  });
 }
 
 $().ready(function(){
   d3.json("../assets/dataset.json", function(err, data){
     quakeData = data;
+    normalizeFeatures();
+    worldMap.init();
   });
-
-  worldMap.init();
 });
