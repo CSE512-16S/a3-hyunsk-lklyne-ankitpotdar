987bf7cdc1289aa9790555fb4291e187e5fcd2d8
Hyun S Kim <hyunkims@gmail.com>
2016-05-01 12:02:26 -0700

    split codebase into separate files

--- earthquake/js/main.js
+++ earthquake/js/main.js
@@ -1,21 +1,6 @@
-var quakeData = null;
-var features = [];
-
-var worldMap = {
-  svg: null,
-  init: function(){
-    worldMap.dataMap = new Datamap({
-      element: $(".map")[0]
-    });
-    worldMap.drawBubbles();
-  },
-  drawBubbles: function(){
-    worldMap.dataMap.bubbles(features);
-  }
-}
-
-function normalizeFeatures(){
-  _.forEach(quakeData.features, function(val, i){
+function normalizeFeatures(data){
+  var features = []
+  _.forEach(data.features, function(val, i){
     var feature = val.properties;
     feature.id = val.id;
     feature.latitude = val.geometry.coordinates[1];
@@ -26,12 +11,14 @@ function normalizeFeatures(){
 
     features.push(feature);
   });
+
+  return features;
 }
 
 $().ready(function(){
   d3.json("../assets/dataset.json", function(err, data){
-    quakeData = data;
-    normalizeFeatures();
-    worldMap.init();
+    var features;
+    features = normalizeFeatures(data);
+    map.init(features);
   });
 });
